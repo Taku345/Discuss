09290aa8df896afe6f70cd1af54cf2b2c9b31c6f
Taku345 <takuma432@gmail.com>
2023-11-12 16:44:15 +0900

    初回コミット

---
+++ resources/ts/Index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const Index: React.FC = () => {
+  return (
+    <div>
+      Hello World!!!
+    </div>
+  );
+}
+
+
+const container = document.getElementById('index');
+if (container) {
+  const root = createRoot(container);
+  root.render(<Index />);
+}
+
+
+
