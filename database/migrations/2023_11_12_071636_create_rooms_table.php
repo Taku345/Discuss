370e5cf93baceb989f0717e02f911c1dfd85d1e9
Taku345 <takuma432@gmail.com>
2023-11-25 11:13:42 +0900

    DBの初期値設定

--- database/migrations/2023_11_12_071636_create_rooms_table.php
+++ database/migrations/2023_11_12_071636_create_rooms_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,8 +17,8 @@ return new class extends Migration
       $table->foreignId('user_id')->constrained('users');
       $table->string('name');
       $table->text('description')->nullable(true);
-      $table->timestamp('started_at');
-      $table->timestamp('finished_at');
+      $table->timestamp('started_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+      $table->timestamp('finished_at')->default(DB::raw('CURRENT_TIMESTAMP'));
       $table->timestamps();
       $table->softDeletes();
     });
